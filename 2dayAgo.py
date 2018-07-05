22d94d808d4de06852f13351aab3b1b7a604514b
sangilpark <40359670+8hc@users.noreply.github.com>
2018-07-05 11:00:17 +0900

    Update 2dayAgo.py

--- 2dayAgo.py
+++ 2dayAgo.py
@@ -4,3 +4,16 @@ days_ago = datetime.datetime.now() - datetime.timedelta(days=2)
 
 ym = days_ago.strftime('%Y-%m')
 ymd = days_ago.strftime('%Y%m%d')
+
+
+def dayago(tday):
+  daytoago = datetime.datetime.now() - datetime.timedelta(days=tday)
+  day2day = daytoago.strftime('%Y%m%d')
+  time2time = daytoago.strftime('%Y%m%d %H%M%S')
+  print('day  : ' , day2day)
+  print('time : ', time2time)
+  
+  
+  
+#check
+ dayago(2)  <-enter
